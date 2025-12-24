774428d7a47c39ee18f4cb8b0467024032647f02
Aruneswaran-31 <aruneswaran25@gmail.com>
2025-12-24 10:54:47 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,10 @@
 const jwt = require("jsonwebtoken");
-
 module.exports = (req, res, next) => {
   const authHeader = req.header("Authorization");
-
   if (!authHeader) {
     return res.status(401).json("No token, authorization denied");
   }
-
   const token = authHeader.split(" ")[1]; // Bearer <token>
-
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
